dca3adef853388352134756083f87028d1bd60fe
Sunny Choudhary <07sunny.choudhary10@gmail.com>
2023-07-04 00:33:18 +0530

    Build fixes

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -36,46 +36,49 @@ const List = () => {
 				<div className="max-w-screen-2xl mx-auto grid grid-cols-1 gap-4 px-3 md:grid-cols-2 md:gap-6 lg:grid-cols-2 lg:gap-8 lg:px-6 lg:py-12">
 					{Experience_Data.map((item) => {
 						return (
-							<div className="opacity-0 " data-observeforanimation
-							data-class="animate-in-from-right animation-delay-100 animation-fill-mode-[both]">
-							<article
+							<div
 								key={item.heading}
-								className="experience-article bg-port-yellow-light p-4 min-w-[300px] min-h-[200px] rounded-2xl shadow-lg  prose-xl transition-all duration-500 hover:shadow-2xl hover:cursor-pointer"
-								
+								className="opacity-0"
+								data-observeforanimation
+								data-class="animate-in-from-right animation-delay-100 animation-fill-mode-[both]"
 							>
-								<div className="flex flex-col items-start justify-start w-full">
-									<h2 className="m-0 text-port-pink-dark">{item.heading}</h2>
-									<div className="flex flex-row justify-between w-full my-4">
-										<span>{item.date}</span>
-										<span
-											className={`rounded-full py-2 px-6 text-white text-sm h-9  ${
-												item.status === "Ongoing"
-													? "bg-port-yellow-ultra-dark animate-pulse"
-													: "bg-black"
-											}`}
-										>
-											{item.status}
-										</span>
+								<article className="experience-article bg-port-yellow-light p-4 min-w-[300px] min-h-[200px] rounded-2xl shadow-lg  prose-xl transition-all duration-500 hover:shadow-2xl hover:cursor-pointer">
+									<div className="flex flex-col items-start justify-start w-full">
+										<h2 className="m-0 text-port-pink-dark">{item.heading}</h2>
+										<div className="flex flex-row justify-between w-full my-4">
+											<span>{item.date}</span>
+											<span
+												className={`rounded-full py-2 px-6 text-white text-sm h-9  ${
+													item.status === "Ongoing"
+														? "bg-port-yellow-ultra-dark animate-pulse"
+														: "bg-black"
+												}`}
+											>
+												{item.status}
+											</span>
+										</div>
+										<p className="font-normal text-base leading-8 text-port-pink-dark lg:text-lg lg:leading-8 hover:cursor-default">
+											{item.description}
+										</p>
+										<div className="details hidden flex-col gap-5 m-4">
+											<img
+												width={250}
+												src={item.details.logo}
+												alt={item.heading}
+											></img>
+											{item.details.paragraph.map((item, i) => {
+												return (
+													<p
+														key={i}
+														className="font-normal text-base m-0 leading-8 text-port-pink-dark lg:text-lg lg:leading-8 hover:cursor-default"
+													>
+														{item}
+													</p>
+												);
+											})}
+										</div>
 									</div>
-									<p className="font-normal text-base leading-8 text-port-pink-dark lg:text-lg lg:leading-8 hover:cursor-default">
-										{item.description}
-									</p>
-									<div className="details hidden flex-col gap-5 m-4">
-										<img
-											width={250}
-											src={item.details.logo}
-											alt={item.heading}
-										></img>
-										{item.details.paragraph.map((item, i) => {
-											return (
-												<p key={i} className="font-normal text-base m-0 leading-8 text-port-pink-dark lg:text-lg lg:leading-8 hover:cursor-default">
-													{item}
-												</p>
-											);
-										})}
-									</div>
-								</div>
-							</article>
+								</article>
 							</div>
 						);
 					})}
