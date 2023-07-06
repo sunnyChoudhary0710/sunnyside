ac211104b71673a444c869f69ea666557438f0e7
sunny-nagarro <sunny.choudhary@nagarro.com>
2023-07-07 01:58:49 +0530

    Updated home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,16 @@ import Header from "@/components/header/header";
 import Overview from "@/components/overview/overview";
 import List from "@/components/list/list";
 import Footer from "@/components/footer/footer";
-import Email from "@/components/emailtemplate/email";
 
 export default function Home() {
 	return (
 		<>
-		<Email name="Sunny"/>
-			{/* <div className="relative font-sans bg-port-yellow-light overflow-x-hidden">
+			<div className="relative font-sans bg-port-yellow-light overflow-x-hidden">
 				<Header />
 				<Overview />
 			</div>
 			<List />
-			<Footer /> */}
+			<Footer />
 		</>
 	);
 }
