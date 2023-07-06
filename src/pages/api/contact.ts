fea904e485a4afe15e6610d9c9a6f97ee4321fc0
sunny-nagarro <sunny.choudhary@nagarro.com>
2023-07-06 22:45:47 +0530

    Added promise for email in prod

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 
-const Contact = (req: any, res: any) => {
+const Contact = async (req: any, res: any) => {
 	try {
 		const data = req.body;
 		let transporter = nodemailer.createTransport({
@@ -15,6 +15,20 @@ const Contact = (req: any, res: any) => {
 			},
 			secure: false,
 		});
+
+		await new Promise((resolve, reject) => {
+			// verify connection configuration
+			transporter.verify(function (error: any, success: any) {
+				if (error) {
+					console.log(error);
+					reject(error);
+				} else {
+					console.log("Server is ready to take our messages");
+					resolve(success);
+				}
+			});
+		});
+
 		const mailData = {
 			from: data.email,
 			to: "sunny@devsunny.in",
@@ -23,45 +37,64 @@ const Contact = (req: any, res: any) => {
 			text: `${data.name} wants to reach out. Email id is ${data.email}. Here's the message.../n ${data.message}`,
 			html: `<p style='margin:10px;background-color:"pink";color:"white"'>${data.message}</p>`,
 		};
-		console.log({
-			port: process.env.SMTP_PORT,
-			host: process.env.SMTP_HOST,
-			auth: {
-				user: process.env.USERNAME,
-				pass: process.env.PASSWORD,
-			},
-			tls: {
-				ciphers: process.env.CIPHER,
-			},
-			secure: false,
-		});
-		console.log(mailData);
-		transporter.sendMail(mailData, function (err: any, info: any) {
-			if (err) {
-				res.end("fail");
-				console.log(err);
-			} else {
-				if (info.rejected && info.rejected.length === 0) {
-					console.log(
-						"++++++++++++++++++Email received. Initiating Welcome email to sender+++++++++++++++++++++"
-					);
-					fetch("https://www.devsunny.in/api/mail", {
-						method: "POST",
-						body: JSON.stringify(data),
-						headers: {
-							"Content-Type": "application/json",
-							Accept: "application/json",
-						},
-					})
-						.then((data) => {
-							console.log(data);
+
+		await new Promise((resolve, reject) => {
+			// send mail
+			transporter.sendMail(mailData, (err: any, info: any) => {
+				if (err) {
+					console.error(err);
+					reject(err);
+				} else {
+					if (info.rejected && info.rejected.length === 0) {
+						console.log(
+							"++++++++++++++++++Email received. Initiating Welcome email to sender+++++++++++++++++++++"
+						);
+						fetch("https://www.devsunny.in/api/mail", {
+							method: "POST",
+							body: JSON.stringify(data),
+							headers: {
+								"Content-Type": "application/json",
+								Accept: "application/json",
+							},
 						})
-						.then((error) => {
-							console.log(error);
-						});
+							.then((data) => {
+								console.log(data);
+							})
+							.then((error) => {
+								console.log(error);
+							});
+					}
+					resolve(info);
 				}
-			}
+			});
 		});
+
+		// transporter.sendMail(mailData, function (err: any, info: any) {
+		// 	if (err) {
+		// 		res.end("fail");
+		// 		console.log(err);
+		// 	} else {
+		// 		if (info.rejected && info.rejected.length === 0) {
+		// 			console.log(
+		// 				"++++++++++++++++++Email received. Initiating Welcome email to sender+++++++++++++++++++++"
+		// 			);
+		// 			fetch("https://www.devsunny.in/api/mail", {
+		// 				method: "POST",
+		// 				body: JSON.stringify(data),
+		// 				headers: {
+		// 					"Content-Type": "application/json",
+		// 					Accept: "application/json",
+		// 				},
+		// 			})
+		// 				.then((data) => {
+		// 					console.log(data);
+		// 				})
+		// 				.then((error) => {
+		// 					console.log(error);
+		// 				});
+		// 		}
+		// 	}
+		// });
 		res.send("success");
 	} catch (error) {
 		console.log(error);
